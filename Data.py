c6082930d62a6bf0090628d26e09e14c889a8baf
fjbautistas <fjbautistas@unal.edu.co>
2020-11-15 18:13:46 -0500

    data trataiment to get the varibles of formalism

--- Data.py
+++ Data.py
@@ -17,17 +17,32 @@ dl=pd.read_csv('data/proc_lo_pert.csv',index_col=None, usecols = ["ms","md","met
                                                                   "massefficiency","nplanets", "ngi"]); 
 dh=pd.read_csv('data/proc_hi_pert.csv',index_col=None, usecols = ["ms","md","metal","taugas","com",
                                                                   "massefficiency","nplanets", "ngi"]); 
+#--------total rocky planets -----
+dn = dn.assign(npt = dn["nplanets"]-dn["ngi"])
+dl = dl.assign(npt = dl["nplanets"]-dl["ngi"])
+dh = dh.assign(npt = dh["nplanets"]-dh["ngi"])
 
-#------- total mass no perturbations-------
+#--------- total masss -----------
 dn = dn.assign(Mtp = dn["massefficiency"]*dn["md"])
-#------- total mass -------
+dl = dl.assign(Mtp = dl["massefficiency"]*dl["md"])
+dh = dh.assign(Mtp = dh["massefficiency"]*dh["md"])
 
+#------- total giant mass -------
 dn = dn.assign(Mjup = ((dn["ngi"]*dn["Mtp"]).divide(dn["nplanets"]))*mass_jup)
+dl = dl.assign(Mjup = ((dl["ngi"]*dl["Mtp"]).divide(dl["nplanets"]))*mass_jup)
+dh = dh.assign(Mjup = ((dh["ngi"]*dh["Mtp"]).divide(dh["nplanets"]))*mass_jup)
 
+#-------- total rocky mass --------
 dn = dn.assign(Mrock = (dn["Mtp"]-(dn["Mjup"]*(mass*10**(-4))))*mass_earth)
+dl = dl.assign(Mrock = (dl["Mtp"]-(dl["Mjup"]*(mass*10**(-4))))*mass_earth)
+dh = dh.assign(Mrock = (dh["Mtp"]-(dh["Mjup"]*(mass*10**(-4))))*mass_earth)
+
+#---- delete the mass efficiency --------
+dn = dn.drop(["massefficiency"], axis=1)
+dl = dl.drop(["massefficiency"], axis=1)
+dh = dh.drop(["massefficiency"], axis=1)
 
 
-
-
-dl = dn.assign(Mtp = dl["massefficiency"]*dl["md"])
-dh = dn.assign(Mtp = dh["massefficiency"]*dh["md"])
+dn.to_csv(r'data/no_p.csv', index = False)
+dl.to_csv(r'data/ow_p.csv', index = False)
+dh.to_csv(r'data/high_p.csv', index = False)
