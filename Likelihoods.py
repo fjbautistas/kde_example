ac759f53863811cea6b91600ed4bd8af188a5606
fjbautistas <fjbautistas@unal.edu.co>
2020-11-15 22:00:35 -0500

    likelihoods

--- Likelihoods.py
+++ Likelihoods.py
@@ -53,7 +53,7 @@ class optimal_pdf(object):
         self.scores, kde_i = [], []
     
         self.pdf_std = np.exp(self.grid_std.best_estimator_.score_samples(self.space_std).reshape(self.std_grid[0].shape))        
-        return self.pdf_std
+        return self.pdf_std.ravel()
 
 #============================================================================================================    
 #================================================ Likelihoods ===============================================
@@ -61,25 +61,28 @@ class optimal_pdf(object):
 
 # md
 md_n = optimal_pdf(dn.ms, dn.metal, dn.md); like_md_n = md_n.pdf();
-
-#likelihoods = {'md_n':}
-
-
-
-
-'''
 md_l = optimal_pdf(dl.ms, dl.metal, dl.md); like_md_l = md_l.pdf(); 
-md_h = optimal_pdf(dh.ms, dh.metal, dh.md); like_md_h = md_h.pdf(); 
+md_h = optimal_pdf(dh.ms, dh.metal, dh.md); like_md_h = md_h.pdf();
 
+likelihoods_md = pd.DataFrame({'md_n':like_md_n, 'md_l':like_md_l, 'md_h':like_md_h})
+likelihoods_md.to_csv("data/like_md.csv")
+'''
 # com
 com_n = optimal_pdf(dn.ms, dn.metal, dn.com); like_com_n = com_n.pdf(); 
 com_l = optimal_pdf(dl.ms, dl.metal, dl.com); like_com_l = com_l.pdf(); 
 com_h = optimal_pdf(dh.ms, dh.metal, dh.com); like_com_h = com_h.pdf(); 
 
+likelihoods_com = pd.DataFrame({'com_n':like_com_n, 'com_l':like_com_l, 'com_h':like_com_h})
+likelihoods_com.to_csv("data/like_com.csv")
+
+
 # Mtp total mass
 Mtp_n = optimal_pdf(dn.ms, dn.metal, dn.Mtp); like_Mtp_n = Mtp_n.pdf(); 
 Mtp_l = optimal_pdf(dl.ms, dl.metal, dl.Mtp); like_Mtp_l =_Mtp_l.pdf(); 
 Mtp_h = optimal_pdf(dh.ms, dh.metal, dh.Mtp); like_Mtp_h = Mtp_h.pdf(); 
+
+likelihoods_Mtp = pd.DataFrame({'Mtp_n':like_Mtp_n, 'Mtp_l':like_Mtp_l, 'Mtp_h':like_Mtp_h})
+likelihoods_Mtp.to_csv("data/like_com.csv")
 
 # Mjup total mass
 Mjup_n = optimal_pdf(dn.ms, dn.metal, dn.Mjup); like_Mjup_n = Mjup_n.pdf(); 
@@ -91,10 +94,16 @@ Mrock_n = optimal_pdf(dn.ms, dn.metal, dn.Mrock); like_Mrock_n = Mrock_n.pdf();
 Mrock_l = optimal_pdf(dl.ms, dl.metal, dl.Mrock); like_Mrock_l = Mrock_l.pdf(); 
 Mrock_h = optimal_pdf(dh.ms, dh.metal, dh.Mrock); like_Mrock_h = Mrock_h.pdf();
 
+likelihoods_Mrock = pd.DataFrame({'Mrock_n':like_Mrock_n, 'Mrock_l':like_Mrock_l, 'Mrock_h':like_Mrock_h})
+likelihoods_Mrock.to_csv("data/like_Mrock.csv")
+
 # Mrock total mass
 nplanets_n = optimal_pdf(dn.ms, dn.metal, dn.nplanets); like_nplanets_n = nplanets_n.pdf(); 
 nplanets_l = optimal_pdf(dl.ms, dl.metal, dl.nplanets); like_nplanets_l = nplanets_l.pdf(); 
 nplanets_h = optimal_pdf(dh.ms, dh.metal, dh.nplanets); like_nplanets_h = nplanets_h.pdf();
+
+likelihoods_nplanets = pd.DataFrame({'nplanets_n':like_nplanets_n, 'nplanets_l':like_nplanets_l, 'nplanets_h':like_nplanets_h})
+likelihoods_nplanets.to_csv("data/like_nplanets.csv")
 
 # Njup total mass
 ngi_n = optimal_pdf(dn.ms, dn.metal, dn.ngi); like_ngi_n = ngi_n.pdf(); 
